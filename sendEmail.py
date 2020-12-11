255505cf8eeb1a6f3769a9b4183baa23be02e5d9
SakethNannaka <nannakasaisaketh@msitprogram.net>
2020-12-12 03:37:37 +0530

    removed try_except as it's conflicting as user succesfully registered in the main app

--- sendEmail.py
+++ sendEmail.py
@@ -251,8 +251,8 @@ def send_email(receiver_email,token_url):
     message.attach(part2)
     
     context = ssl.create_default_context()
-    try:
-        with smtplib.SMTP(smtp_server, port) as server:
+    # try:
+    with smtplib.SMTP(smtp_server, port) as server:
         # Extended HELO (EHLO) is an Extended Simple Mail Transfer Protocol (ESMTP) command sent by an email server to identify itself when connecting to another email server to start the process of sending an email. ... The EHLO command tells the receiving server it supports extensions compatible with ESMTP.
             server.ehlo()  # Can be omitted
             server.starttls(context=context)
@@ -260,5 +260,5 @@ def send_email(receiver_email,token_url):
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
             server.close()
-    except:
-        print("login/email sending failed")
+    # except:
+        # print("login/email sending failed")
